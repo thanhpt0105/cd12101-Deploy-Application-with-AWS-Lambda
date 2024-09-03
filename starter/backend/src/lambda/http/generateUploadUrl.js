d71ff0099408907ca4c44221a2a187c1a086e87b
thanhpt0105 <thanhpt0105@gmail.com>
2024-09-04 04:31:14 +0800

    Add s3 resource

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,25 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { generateUploadUrl } from '../../businessLogic/todos.js'
+import { getUserId } from '../utils.mjs'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(cors({
+    credentials: true
+  }))
+  .handler(async (event) => {
+    const userId = getUserId(event)
+    const todoId = event.pathParameters.todoId
+    const uploadUrl = await generateUploadUrl(userId, todoId)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  })
+
 
